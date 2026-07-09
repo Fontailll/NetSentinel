3afc87a8e263d9f80370fe181cacf7c0a3cdf0d1
Fontail <301758411+Fontailll@users.noreply.github.com>
2026-07-09 19:07:29 +0300

    Delete platform directory

--- platform/linux/lin_proc.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <optional>
-#include <cstdint>
-#include "../../include/process.h"
-
-std::optional<ProcessInfo> get_process_info(uint32_t pid);
--- platform/linux/signature.cpp
+++
@@ -1,9 +0,0 @@
-#include "signature.h"
-
-std::optional<SignatureInfo> verify_signature(const std::string& path) {
-    (void)path;
-    SignatureInfo info;
-    info.signed_binary = true;
-    info.publisher = "Linux";
-    return info;
-}
